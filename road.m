f4e3aef2da107cd8e43dd656c04d8bd4ef4501e2
lcl1026504480 <17863130570@163.com>
2019-01-28 09:49:21 +0800

    道路情况

--- road.m
+++ road.m
@@ -1,6 +1,8 @@
 %% I. 清空环境
 clc
 clear
+Road=imread('p1.bmp');
+Road=logical(Road);
 x1=[67.05 66.90 66.74 66.47 66.33 66.25 65.93];%南边的插值
 y1=[17.97 17.95 17.99 17.99 17.98 17.93 17.98];
 xi1=67.05:-0.01:65.93;
@@ -27,9 +29,10 @@ xi11=67.18:0.01:67.05;
 yi11 = interp1(x11,y11,xi11, 'cubic');
 x5=[65.65 66.13 66.567];
 y5=[18.33 18.292 18.43];
-jindu=[xi1,xi4,xi8,xi7,xi10,67.05];
+jindu=-[xi1,xi4,xi8,xi7,xi10,67.05];
 weidu=[yi1,yi4,yi8,yi7,yi10,17.97];
-py=[65.65 66.03 66.07 66.16 66.73];%投放点经度
+
+py=-[65.65 66.03 66.07 66.16 66.73];%投放点经度
 px=[18.33 18.22 18.44 18.40 18.47];%投放点纬度
 pc=[5 7 4 12 2];%投放点物资负重
 dm=[3.5 8 14 11 15 22 20];%飞机最大负重
@@ -39,8 +42,6 @@ p3=[0 1 1 1 1 1 0];
 p4=[0 0 1 0 1 1 0];
 p5=[1 1 1 1 1 1 0];
 p=[p1;p2;p3;p4;p5];%投放点和无人机关系
-py=[65.65 66.03 66.07 66.16 66.73];%投放点经度
-px=[18.33 18.22 18.44 18.40 18.47];%投放点纬度
 % %% II. 绘制目标函数曲线
 % figure
 % [x,y] = meshgrid(-5:0.1:5,-5:0.1:5);
@@ -61,18 +62,18 @@ Vymax=0.8;
 Vymin=-0.8;
 popxmax = 18.5;
 popxmin = 17.9;
-popymax=67.2;
-popymin=65.6;
+popymin=-67.2;
+popymax=-65.6;
 ws=0.9;
 we=0.4;
 %% IV. 产生初始粒子和速度
 for i = 1:sizepop
     % 随机产生一个种群
     pop(i,1:3) = 0.3*rands(1,3)+18.2; 
-    pop(i,4:6) = 0.8*rands(1,3)+66.4; %初始种群
+    pop(i,4:6) = -(0.8*rands(1,3)+66.4); %初始种群
     V(i,:) = rands(1,6);  %初始化速度
     % 计算适应度
-    fitness(i) = fun(pop(i,:),pc,dm,p,jindu,weidu);   %染色体的适应度
+    [fitness(i),pf,dt] = roadfun(pop(i,:),pc,dm,p,jindu,weidu,Road);   %染色体的适应度
 end
 
 %% V. 个体极值和群体极值
@@ -100,7 +101,7 @@ for i = 1:maxgen
         pop(j,pop(j,4:6)>popymax) = popymax;
         pop(j,pop(j,4:6)<popymin) = popymin;
         % 适应度值更新
-        fitness(j) = fun(pop(j,:),pc,dm,p,jindu,weidu); 
+        [fitness(j),pf,dt] = roadfun(pop(j,:),pc,dm,p,jindu,weidu,Road); 
     end
     
     for j = 1:sizepop  
@@ -125,14 +126,15 @@ plot(yy)
 title('Optimal Individual Fitness','fontsize',12);
 xlabel('iterations','fontsize',12);ylabel('fitness','fontsize',12);
 figure
-global dt
-global pf
+plot(jindu,weidu);
+xlabel('Latitude','fontsize',12);ylabel('Longitude','fontsize',12);
+hold on
 dt
 zx=zbest(1:3);
 zy=zbest(4:6);
 scatter(py,px,'filled')
 xlabel('Latitude','fontsize',12);ylabel('Longitude','fontsize',12);
-if sum(pf)==5 && 
+if sum(pf)==5 && fitnesszbest>0
     for i=1:3
         if sum(dt(i,:))~=0
             hold on
@@ -141,20 +143,22 @@ if sum(pf)==5 &&
         end
     end
     grid
-    legend('Delivery Locations','cargo container1','cargo container2','cargo container3',...
-       'route 1','route 2','route 3','route 4','route 5','route 6')
 end
 for i=1:5
     text(py(i)+0.01,px(i)+0.01,['p',num2str(i)])
 end
-for i=1:3
-    for j=1:5
-         if dt(i,j)~=0
-             ri=mod(dt(i,j),10);
-             hold on
-             plot([zy(i),py(j),zy(ri)],[zx(i),px(j),zx(ri)])
+if sum(pf)==5 && fitnesszbest>0
+    for i=1:3
+        for j=1:5
+             if dt(i,j)~=0
+                 ri=mod(dt(i,j),10);
+                 hold on
+                 plot([zy(i),py(j),zy(ri)],[zx(i),px(j),zx(ri)])
+            end
         end
     end
+    legend('borders','Delivery Locations','cargo container1','cargo container2','cargo container3',...
+       'route 1','route 2','route 3','route 4','route 5')
 end
 
 
---
+++ roadfun.m
@@ -0,0 +1,111 @@
+function [y,pf,dt] = roadfun(x,pc,dm,p,jindu,weidu,Rin)
+                                    global b
+                                    global y1
+                                    global x1
+                                    global x2
+                                    global y2
+py=-[65.65 66.03 66.07 66.16 66.73];%投放点经度
+px=[18.33 18.22 18.44 18.40 18.47];%投放点纬度
+dd=[70/3 158/3 112/3 18 15 158/5 17.07];%无人机最大飞行距离
+R=6371;%地球半径
+dx=R*pi/180;%纬度单位距离
+dy=2*R*cosd(mean(px))*pi/360;%经度单位距离
+f=@(od,ml,p,cl) od*(1-cl*p/ml);%负重对于飞行距离影响的函数
+dis=@(x1,y1,x2,y2) sqrt(((x1-x2)*dx)^2+((y1-y2)*dy)^2);%计算距离函数
+% dp=zeros(5,5);%投放点之间的距离
+sop=0;%重合像素个数
+pid=zeros(3,5);%投放点和ISO点距离
+dis=@(x1,y1,x2,y2) sqrt(((x1-x2)*dx).^2+((y1-y2)*dy).^2);%计算距离函数f
+fx=@(x1) (284.505-260.84)/(-67.15+67.19)*(x1+67.19)+260.84;
+fy=@(y1) (162.4-560.583)/(18.5-17.93)*(y1-17.93)+560.583;
+fx1=@(x1) (284.505-260.84)\(-67.15+67.19)*(x1-260.84)-67.19;
+fy1=@(y1) (162.4-560.583)\(18.5-17.93)*(y1-560.583)+17.93;
+for i=1:3
+    for j=1:5
+        pid(i,j)=dis(x(i),x(i+3),px(j),py(j));
+    end
+end
+pf=zeros(1,5);%投放点标志
+dt=zeros(3,5);%ISO和无人机类型的关系
+xp=round(fy(x(1:3)));
+yp=round(fx(x(4:6)));
+pxp=round(fy(px));
+pyp=round(fx(py));
+% for i=1:5
+%     for j=1:i
+%         dp(i,j)=dis(px(i),py(i),px(j),py(j));
+%         dp(j,i)=dp(i,j);
+%     end
+% end 
+    for i=1:3
+        for j=1:5
+            for k=1:7
+                if p(j,k)==1 && pid(i,j)<f(dd(k),dm(k),pc(j),0.4)
+                    for n=1:3
+                        if pid(n,j)+pid(i,j)<f(dd(k),dm(k),pc(j),0.4)
+                            if pf(j)==0
+                                dt(i,j)=k*10+n;
+                                pf(j)=1;
+                                if k~=6
+                                    b=false(size(Rin));
+                                    w=zeros(size(Rin));
+                                    if pxp(j)~=xp(i) && pxp(j)~=xp(n)
+                                        x1=xp(i):sign(pxp(j)-xp(i)):pxp(j);
+                                        y1=round((yp(i)-pyp(j))/(xp(i)-pxp(j))*(x1-xp(i))+yp(i));
+                                        x2=xp(n):sign(pxp(j)-xp(n)):pxp(j);
+                                        y2=round((yp(n)-pyp(j))/(xp(n)-pxp(j))*(x2-xp(n))+yp(n));
+                                         for l=1:length(x1)
+                                            b(x1(l),y1(l))=1;
+                                            w(x1(l),y1(l))=sum(exp(-dis(fx1(x1(l)),fy1(y1(l)),px,py)));
+                                         end
+                                        for l=1:length(x2)
+                                            b(x2(l),y2(l))=1;
+                                            w(x2(l),y2(l))=sum(exp(-dis(fx1(x2(l)),fy1(y2(l)),px,py)));
+                                        end
+                                    else
+                                        if pxp(j)==xp(i) && pxp(j)~=xp(n)
+                                            x1=xp(i);
+                                            y1=yp(i):sign(pyp(j)-yp(i)):pyp(j);
+                                            x2=xp(n):sign(pxp(j)-xp(n)):pxp(j);
+                                            y2=round((yp(n)-pyp(j))/(xp(n)-pxp(j))*(x2-xp(n))+yp(n));
+                                            b(x1,y1)=1;
+                                        else
+                                            if  pxp(j)==xp(n) && pxp(j)~=xp(i)
+                                                x1=xp(i):sign(pxp(j)-xp(i)):pxp(j);
+                                                y1=round((yp(i)-pyp(j))/(xp(i)-pxp(j))*(x1-xp(i))+yp(i));
+                                                x2=xp(n);
+                                                y2=yp(n):sign(pyp(j)-yp(n)):pyp(j);
+                                                b(x2,y2)=1;
+                                            end
+                                        end
+                                    end
+
+                                    Road1=Rin & b;
+                                    sop=sop+sum(sum(Road1.*w));
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+zx=x(1:3);
+zy=x(4:6);
+ip=inpolygon(zy,zx,jindu,weidu);
+if min(min(pid))>3
+    y=sop*fix(sum(pf)/5)*fix(sum((ip/3)));
+else
+    y=0;
+end
+end
+%             
+% scatter(px,py,'filled')
+% for i=1:5
+%     text(px(i)+0.01,py(i)+0.01,['p',num2str(i)])
+% end
+% y = x(1).^2 + x(2).^2 - 10*cos(2*pi*x(1)) - 10*cos(2*pi*x(2)) + 20;
+
+
+
+
